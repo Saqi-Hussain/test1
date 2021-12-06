d4bebd9975fba40631b2d6039d11bd1bda6e6f19
Saqi-Hussain <92258621+Saqi-Hussain@users.noreply.github.com>
2021-12-06 10:17:24 +0500

    FirstOne

---
+++ app/src/main/java/com/example/test1/screen2.kt
@@ -0,0 +1,11 @@
+package com.example.test1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class screen2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screen2)
+    }
+}
